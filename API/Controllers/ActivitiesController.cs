b16c14a23937dcb54f6cf1b77ebccd9378f232c3
Sergio Pastor Ontiveros Perez <sergio.perez@meta.com.br>
2022-07-16 17:59:52 -0300

    End of section 4

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,15 +1,41 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Application.Activities;
+using Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
 
 namespace API.Controllers
 {
     public class ActivitiesController : BaseApiController
     {
-        public ActivitiesController()
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivitiesAsync()
         {
-            
+            return await Mediator.Send(new List.Query());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> GetActivity(Guid id)
+        {
+            return await Mediator.Send(new Details.Query{Id = id});
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateActivity(Activity activity)
+        {
+            return Ok(await Mediator.Send(new Creates.Command{Activity = activity}));
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> EditActivity(Guid id, Activity activity)
+        {
+            activity.Id = id;
+            return Ok(await Mediator.Send(new Edit.Command{Activity = activity}));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteActivity(Guid id)
+        {
+            return Ok(await Mediator.Send(new Delete.Command{Id = id}));
         }
     }
 }
